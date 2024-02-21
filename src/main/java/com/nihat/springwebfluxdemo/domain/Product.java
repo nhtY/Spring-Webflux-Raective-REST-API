8b7e613752a82572bae9615b06ec63c9b1cc67c1
nhtY <n_hat.yalcin@outlook.com>
2024-02-21 13:11:33 +0300

    Create product domain.

---
+++ src/main/java/com/nihat/springwebfluxdemo/domain/Product.java
@@ -0,0 +1,34 @@
+package com.nihat.springwebfluxdemo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String imgUrl;
+    private BigDecimal price;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
