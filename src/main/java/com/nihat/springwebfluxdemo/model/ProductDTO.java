14fb8acdab8e1c7da38611126b7d7a8f95d7b8d5
nhtY <n_hat.yalcin@outlook.com>
2024-02-21 13:18:08 +0300

    Create product DTO. - annotate the fields with validation constraints.

---
+++ src/main/java/com/nihat/springwebfluxdemo/model/ProductDTO.java
@@ -0,0 +1,43 @@
+package com.nihat.springwebfluxdemo.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDTO {
+
+    private String id;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
+    private String description;
+
+    @NotBlank
+    @URL
+    private String imgUrl;
+
+    @NotNull
+    private BigDecimal price;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
