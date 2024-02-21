a163afbdd9ee3e1dc7b49ed13e9c3fca9ff7d59e
nhtY <n_hat.yalcin@outlook.com>
2024-02-21 12:13:55 +0300

    Init the project.

---
+++ src/main/java/com/nihat/springwebfluxdemo/SpringWebfluxReactiveApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nihat.springwebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReactiveApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReactiveApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nihat/springwebfluxdemo/SpringWebfluxReactiveApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nihat.springwebfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxReactiveApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
