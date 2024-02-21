467931bcd9d798bb3dd052cc7a60baba6fd1f56b
nhtY <n_hat.yalcin@outlook.com>
2024-02-21 23:23:52 +0300

    Implement the service method deleteProductById.

--- src/main/java/com/nihat/springwebfluxdemo/services/ProductServiceImpl.java
+++ src/main/java/com/nihat/springwebfluxdemo/services/ProductServiceImpl.java
@@ -50,6 +50,6 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Mono<Void> deleteProductById(String id) {
-        return null;
+        return productRepository.deleteById(id);
     }
 }
