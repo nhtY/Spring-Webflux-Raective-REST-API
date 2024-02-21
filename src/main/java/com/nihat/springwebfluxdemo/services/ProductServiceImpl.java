e89d0ac41a69b42291a55f6c01ef692cae81d496
nhtY <n_hat.yalcin@outlook.com>
2024-02-21 15:58:26 +0300

    Inject product repository and mapper.

--- src/main/java/com/nihat/springwebfluxdemo/services/ProductServiceImpl.java
+++ src/main/java/com/nihat/springwebfluxdemo/services/ProductServiceImpl.java
@@ -1,12 +1,20 @@
 package com.nihat.springwebfluxdemo.services;
 
+import com.nihat.springwebfluxdemo.mappers.ProductMapper;
 import com.nihat.springwebfluxdemo.model.ProductDTO;
+import com.nihat.springwebfluxdemo.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
     @Override
     public Flux<ProductDTO> getAllProducts() {
         return null;
