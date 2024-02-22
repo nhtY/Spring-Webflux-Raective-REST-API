58391ebbea6c15737a344a71f311753c6d275f2a
nhtY <n_hat.yalcin@outlook.com>
2024-02-22 17:35:28 +0300

    Test mvc getAll.

--- src/test/java/com/nihat/springwebfluxdemo/controllers/mvc/ProductControllerTest.java
+++ src/test/java/com/nihat/springwebfluxdemo/controllers/mvc/ProductControllerTest.java
@@ -3,13 +3,14 @@ package com.nihat.springwebfluxdemo.controllers.mvc;
 import com.nihat.springwebfluxdemo.model.ProductDTO;
 import com.nihat.springwebfluxdemo.services.ProductService;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -21,6 +22,18 @@ class ProductControllerTest {
 
     @Autowired
     ProductService productService;
+
+    @Test
+    @Order(1)
+    void testGetAllProducts() {
+        // When
+        webTestClient.get().uri(ProductController.PRODUCT_BASE_URL)
+                .exchange()
+                // Then
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(ProductDTO.class).hasSize(4); // initially loaded + testProduct
+    }
 
 
     private ProductDTO getSavedTestProduct() {
