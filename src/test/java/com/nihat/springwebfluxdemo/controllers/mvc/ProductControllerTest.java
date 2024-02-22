b08de4efc29e1217ca3b44fc7951c94819ce148b
nhtY <n_hat.yalcin@outlook.com>
2024-02-22 17:37:08 +0300

    Test mvc createProduct.

--- src/test/java/com/nihat/springwebfluxdemo/controllers/mvc/ProductControllerTest.java
+++ src/test/java/com/nihat/springwebfluxdemo/controllers/mvc/ProductControllerTest.java
@@ -11,6 +11,11 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -63,6 +68,47 @@ class ProductControllerTest {
                 .expectStatus().isNotFound();
     }
 
+    @Test
+    @Order(4)
+    void testCreateProduct() {
+        // Given
+        ProductDTO newProductDTO = new ProductDTO();
+        newProductDTO.setName("New Product");
+        newProductDTO.setDescription("New Product");
+        newProductDTO.setImgUrl("https://image.com/image.jpeg");
+        newProductDTO.setPrice(BigDecimal.TEN);
+        // Set other fields as needed
+
+        // When
+        webTestClient.post().uri(ProductController.PRODUCT_BASE_URL)
+                .body(Mono.just(newProductDTO), ProductDTO.class)
+                .exchange()
+                // Then
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody(ProductDTO.class)
+                .value(created -> {
+                    assertThat(created.getName().equals(newProductDTO.getName())).isTrue();
+                }); // Define expectedProductDTO based on test data
+    }
+
+    @Test
+    void testCreateProductBadRequest() {
+        // Given
+        ProductDTO invalidProductDTO = new ProductDTO();
+        invalidProductDTO.setName(null); // Set name to null to simulate a bad request
+        invalidProductDTO.setImgUrl("https://image.com/image.jpeg");
+        invalidProductDTO.setDescription("New Product");
+        invalidProductDTO.setPrice(BigDecimal.TEN);
+        // Set other fields as needed
+
+        // When
+        webTestClient.post().uri(ProductController.PRODUCT_BASE_URL)
+                .body(Mono.just(invalidProductDTO), ProductDTO.class)
+                .exchange()
+                // Then
+                .expectStatus().isBadRequest();
+    }
 
     private ProductDTO getSavedTestProduct() {
         return productService.getAllProducts().next().block();
